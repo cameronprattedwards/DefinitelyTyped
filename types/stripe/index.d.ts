8eaca8ad950dc730bdb9fa8b1b712ae70dbf1b34
Cameron Edwards <cameronedwards@Lauras-MacBook-Air.local>
2017-05-23 13:48:18 -0400

    Start on Stripe interface declaration

--- types/stripe/index.d.ts
+++ types/stripe/index.d.ts
@@ -1,167 +1,236 @@
-// Type definitions for stripe 2.x
+// Type definitions for stripe 4.x
 // Project: https://stripe.com/
 // Definitions by: Andy Hawkins <https://github.com/a904guy/,http://a904guy.com>
 //                 Eric J. Smith <https://github.com/ejsmith/>
 //                 Amrit Kahlon <https://github.com/amritk/>
 //                 Adam Cmiel <https://github.com/adamcmiel>
 //                 Justin Leider <https://github.com/jleider>
+//                 Cameron Edwards <https://github.com/cameronprattedwards>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare const Stripe: StripeStatic;
+export = stripe;
+export as namespace stripe;
 
-interface StripeStatic {
-    applePay: StripeApplePay;
-    setPublishableKey(key: string): void;
-    validateCardNumber(cardNumber: string): boolean;
-    validateExpiry(month: string, year: string): boolean;
-    validateCVC(cardCVC: string): boolean;
-    cardType(cardNumber: string): StripeCardDataBrand;
-    getToken(token: string, responseHandler: (status: number, response: StripeCardTokenResponse) => void): void;
-    card: StripeCard;
-    createToken(data: StripeCardTokenData, responseHandler: (status: number, response: StripeCardTokenResponse) => void): void;
-    bankAccount: StripeBankAccount;
-}
+declare namespace stripe {
+    interface PaginationRequest {
+        limit?: number;
+        starting_after?: number;
+        ending_before?: number;
+    }
 
-interface StripeCardTokenData {
-    number: string;
-    exp_month?: number;
-    exp_year?: number;
-    exp?: string;
-    cvc?: string;
-    name?: string;
-    address_line1?: string;
-    address_line2?: string;
-    address_city?: string;
-    address_state?: string;
-    address_zip?: string;
-    address_country?: string;
-}
+    interface CurrencyBalance {
+        currency: string;
+        amount: number;
+        source_types: any;
+    }
 
-interface StripeTokenResponse {
-    id: string;
-    client_ip: string;
-    created: number;
-    livemode: boolean;
-    object: string;
-    type: string;
-    used: boolean;
-    error?: StripeError;
-}
+    interface Balance {
+        object: "balance_transaction";
+        available: CurrencyBalance[];
+        livemode: boolean;
+        pending: CurrencyBalance[];
+    }
 
-interface StripeCardTokenResponse extends StripeTokenResponse {
-    card: StripeCard;
-}
+    interface FeeDetail {
+        amount: number;
+        application: string;
+        currency: string;
+        description: string;
+        type: string;
+    }
 
-interface StripeError {
-    type: string;
-    code: string;
-    message: string;
-    param?: string;
-}
+    interface BalanceTransaction {
+        id: string;
+        object: "balance_transaction";
+        amount: number;
+        available_on: number;
+        created: number;
+        currency: string;
+        description: string;
+        fee: number;
+        fee_details: FeeDetail[];
+        net: number;
+        source: string;
+        status: string;
+        type: string;
+    }
 
-type StripeCardDataBrand = 'Visa' | 'American Express' | 'MasterCard' | 'Discover' | 'JCB' | 'Diners Club' | 'Unknown';
+    interface ChargeOutcome {
+        network_status: string;
+        reason: string;
+        risk_level: string;
+        rule: string;
+        seller_message: string;
+        type: string;
+    }
 
-interface StripeCard {
-    object: string;
-    last4: string;
-    exp_month: number;
-    exp_year: number;
-    country?: string;
-    name?: string;
-    address_line1?: string;
-    address_line2?: string;
-    address_city?: string;
-    address_state?: string;
-    address_zip?: string;
-    address_country?: string;
-    brand?: StripeCardDataBrand;
-    createToken(data: StripeCardTokenData, responseHandler: (status: number, response: StripeCardTokenResponse) => void): void;
-    validateCardNumber(cardNumber: string): boolean;
-    validateExpiry(month: string, year: string): boolean;
-    validateCVC(cardCVC: string): boolean;
-}
+    interface RefundList extends List {
+        data: Refund[];
+    }
 
-interface StripeBankAccount {
-    createToken(params: StripeBankTokenParams, stripeResponseHandler: (status: number, response: StripeBankTokenResponse) => void): void;
-    validateRoutingNumber(routingNumber: number | string, countryCode: string): boolean;
-    validateAccountNumber(accountNumber: number | string, countryCode: string): boolean;
-}
+    interface Refund {
+        id: string;
+        object: "list";
+        amount: number;
+        balance_transaction: string;
+        charge: string;
+        created: number;
+        currency: string;
+        description: string;
+        metadata: any;
+        reason: string;
+        receipt_number: string;
+        status: string;
+    }
 
-interface StripeBankTokenParams {
-    country: string;
-    currency: string;
-    account_number: number | string;
-    routing_number?: number | string;
-    account_holder_name: string;
-    account_holder_type: string;
-}
-
-interface StripeBankTokenResponse extends StripeTokenResponse {
-    bank_account: {
+    interface Address {
+        city: string;
         country: string;
-        bank_name: string;
-        last4: number;
-        validated: boolean;
-        object: string;
-    };
+        line1: string;
+        line2: string;
+        postal_code: string;
+        state: string;
+    }
+
+    interface Shipping {
+        address: Address;
+        carrier: string;
+        name: string;
+        phone: string;
+        tracking_number: string;
+    }
+
+    interface Charge {
+        id: string;
+        object: "charge";
+        amount: number;
+        amount_refunded: number;
+        application: string;
+        application_fee: string;
+        balance_transaction: string;
+        captured: boolean;
+        created: number;
+        currency: string;
+        customer: string;
+        description: string;
+        destination: string;
+        dispute: string;
+        failure_code: string;
+        failure_message: string;
+        fraud_details: any;
+        invoice: string;
+        livemode: boolean;
+        metadata: any;
+        on_behalf_of: string;
+        order: string;
+        outcome: ChargeOutcome;
+        paid: boolean;
+        receipt_email: string;
+        receipt_number: string;
+        refunded: boolean;
+        refunds: RefundList;
+        review: string;
+        shipping: Shipping;
+        source: any;
+        source_transfer: string;
+        statement_descriptor: string;
+        status: string;
+        transfer: string;
+        transfer_group: string;
+    }
+
+    interface Source {
+        exp_month: string;
+        exp_year: string;
+        number: string;
+        object: "card";
+        cvc: string;
+        address_city?: string;
+        address_country?: string;
+        address_line1?: string;
+        address_line2?: string;
+        name?: string;
+        address_state?: string;
+        address_zip?: string;
+    }
+
+    interface CreateChargeRequest {
+        amount: number;
+        currency: string;
+        application_fee?: number;
+        capture?: boolean;
+        description?: string;
+        destination?: any;
+        transfer_group?: string;
+        on_behalf_of?: string;
+        metadata?: {};
+        receipt_email?: string;
+        shipping?: Shipping;
+        customer?: string;
+        source: string;
+        source: Source;
+        statement_descriptor?: string;
+    }
+
+    interface UpdateChargeRequest {
+        description?: string;
+        fraud_details?: any;
+        metadata?: any;
+        receipt_email?: string;
+        shipping?: Shipping;
+        transfer_group?: string;
+    }
+
+    interface CaptureChargeRequest {
+
+    }
+
+    interface Charges {
+        retrieve(id: string): Promise<Charge>;
+        create(CreateChargeRequest): Promise<Charge>;
+        update(id: string, request: UpdateChargeRequest): Promise<Charge>;
+        capture(id: string);
+    }
+
+    interface List {
+        object: "list";
+        url: string;
+        has_more: boolean;
+    }
+
+    interface BalanceTransactionList extends List {
+        data: BalanceTransaction[];
+    }
+
+    interface QueryObject {
+        gt?: string;
+        gte?: string;
+        lt?: string;
+        lte?: string;
+    }
+
+    interface BalanceTransactionListRequest extends PaginationRequest {
+        available_on?: string;
+        available_on?: QueryObject;
+        created?: string;
+        created?: QueryObject;
+        currency?: string;
+        payout?: string;
+        source?: string;
+        type?: string;
+    }
+
+    interface Balances {
+        retrieve(): Promise<Balance>;
+        retrieveTransaction(id: string): Promise<BalanceTransaction>;
+        listTransactions(): Promise<BalanceTransactionList>;
+    }
+
+    interface Stripe {
+        charges: Charges;
+        balance: Balance;
+        setApiVersion(version: string): void;
+    }
+    function stripeFactory(apiKey: string): Stripe;
+    export default stripeFactory;
 }
-
-interface StripeApplePay {
-    checkAvailability(resopnseHandler: (result: boolean) => void): void;
-    buildSession(data: StripeApplePayPaymentRequest,
-                 onSuccessHandler: (result: StripeApplePaySessionResult, completion: ((value: any) => void)) => void,
-                 onErrorHanlder: (error: { message: string }) => void): any;
-}
-
-type StripeApplePayBillingContactField = 'postalAddress' | 'name';
-type StripeApplePayShippingContactField = StripeApplePayBillingContactField | 'phone' | 'email';
-type StripeApplePayShipping = 'shipping' | 'delivery' | 'storePickup' | 'servicePickup';
-
-interface StripeApplePayPaymentRequest {
-    billingContact: StripeApplePayPaymentContact;
-    countryCode: string;
-    currencyCode: string;
-    total: StripeApplePayLineItem;
-    lineItems?: StripeApplePayLineItem[];
-    requiredBillingContactFields?: StripeApplePayBillingContactField[];
-    requiredShippingContactFields?: StripeApplePayShippingContactField[];
-    shippingContact?: StripeApplePayPaymentContact;
-    shippingMethods?: StripeApplePayShippingMethod[];
-    shippingType?: StripeApplePayShipping[];
-}
-
-// https://developer.apple.com/reference/applepayjs/1916082-applepay_js_data_types
-interface StripeApplePayLineItem {
-    type: 'pending' | 'final';
-    label: string;
-    amount: number;
-}
-
-interface StripeApplePaySessionResult {
-    token: StripeCardTokenResponse;
-    shippingContact?: StripeApplePayPaymentContact;
-    shippingMethod?: StripeApplePayShippingMethod;
-}
-
-interface StripeApplePayShippingMethod {
-    label: string;
-    detail: string;
-    amount: number;
-    identifier: string;
-}
-
-interface StripeApplePayPaymentContact {
-    emailAddress: string;
-    phoneNumber: string;
-    givenName: string;
-    familyName: string;
-    addressLines: string[];
-    locality: string;
-    administrativeArea: string;
-    postalCode: string;
-    countryCode: string;
-}
-
-// The Stripe client side APIs are not made available to package managers for direct installation.
-// As explained compliance reasons. Source: https://github.com/stripe/stripe-node/blob/master/README.md#these-are-serverside-bindings-only
-// A release date versioning schema is used to version these APIs.
